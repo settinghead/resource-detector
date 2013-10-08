cb512dabe427b91a0a2b914d2bb826b0ce9c7f62
Xiyang Chen <settinghead@gmail.com>
2013-10-08 13:02:58 +0800

    method refactoring

--- lib/url-identifier.rb
+++ lib/url-identifier.rb
@@ -2,73 +2,18 @@ require "url-identifier/version"
 require 'domainatrix'
 require 'cgi'
 require "addressable/uri"
+require 'ostruct'
 
 module UrlIdentifier
   def analyze(url)
   	#get url components
     duri = ::Domainatrix.parse url
     dduri = Addressable::URI.parse url
-    referral_path = duri.path.empty? ? "/" : duri.path
-    filters = {}
-    parameters = {}
-    parameters = CGI::parse dduri.query unless dduri.query.nil?
 
-    domain = strip_www duri
+    params = get_params(duri, dduri)
 
-    #set the default
-    result = {
-      :source => "#{domain}.#{duri.public_suffix}",
-      :uid => referral_path
-    }
+    parse(params)
 
-    params = {
-      :domain => domain,
-      :path => dduri.path,
-      :query => dduri.query,
-      :public_suffix => duri.public_suffix
-    }
-
-    if duri.domain == "blogspot"
-      result[:source] = "#{domain}"
-      result[:uid] = "#{dduri.path}"
-    elsif duri.domain == "youtube"
-      result[:source] = "#{domain}"
-      unless dduri.query.nil?
-        result[:uid] = parameters["v"].first || result[:uid]
-      end
-    elsif duri.domain == "youtu" and duri.public_suffix == "be"
-      result[:source] = "youtube"
-      m = /\/(.+)$/.match duri.path
-      if m.length > 1 #found v id
-        result[:uid] = m[1] || result[:uid]
-      end
-    elsif duri.domain == "lookbook"
-      result[:source] = "#{domain}"
-      if duri.path.start_with? "/look/"
-        m = /\/look\/([0-9]+).*/.match duri.path
-        if m.length > 1 #found look id
-          result[:uid] = m[1] || result[:uid]
-        end
-      end
-    elsif duri.domain == "fashiolista"
-      result[:source] = "#{domain}"
-      if duri.path.start_with? "/item/"
-        m = /\/item\/([0-9]+).*/.match duri.path
-        if m.length > 1 #found item id
-          result[:uid] = m[1] || result[:uid]
-        end
-      end
-    elsif duri.domain == "shareasale"
-      result[:source] = "#{domain}"
-      unless dduri.query.nil?
-        result[:uid] = parameters["afftrack"].first unless parameters["afftrack"].first.nil? or parameters["afftrack"].first.empty?
-        result[:uid] = (result[:uid].split '--').first
-      end 
-    elsif duri.domain == "facebook" #do not process facebook stats for now
-      result[:source] = "#{domain}"
-      result[:uid] = nil
-    end
-    result
   end
 
   private
@@ -82,7 +27,66 @@ module UrlIdentifier
     else
       domain = "#{duri.subdomain}."
     end
-    domain += duri.domain
-    domain
+    return domain + duri.domain
+  end
+
+  def get_params(duri, dduri)
+
+    OpenStruct.new :domain => strip_www(duri),
+      :main_domain => duri.domain,
+      :path => dduri.path,
+      :query => dduri.query,
+      :query_parameters => (dduri.query.nil? ? {} : CGI::parse(dduri.query)),
+      :public_suffix => duri.public_suffix,
+      :referral_path => duri.path.empty? ? "/" : duri.path
+  end
+
+  def parse(params)
+    #set the default
+    result = {
+      :source => "#{params.domain}.#{params.public_suffix}",
+      :uid => params.referral_path
+    }
+    if params.main_domain == "blogspot"
+      result[:source] = "#{params.domain}"
+      result[:uid] = "#{params.path}"
+    elsif params.main_domain == "youtube"
+      result[:source] = "#{params.main_domain}"
+      unless params.query.nil?
+        result[:uid] = params.query_parameters["v"].first || result[:uid]
+      end
+    elsif params.main_domain == "youtu" and params.public_suffix == "be"
+      result[:source] = "youtube"
+      m = /\/(.+)$/.match params.path
+      if m.length > 1 #found v id
+        result[:uid] = m[1] || result[:uid]
+      end
+    elsif params.main_domain == "lookbook"
+      result[:source] = "#{params.main_domain}"
+      if params.path.start_with? "/look/"
+        m = /\/look\/([0-9]+).*/.match params.path
+        if m.length > 1 #found look id
+          result[:uid] = m[1] || result[:uid]
+        end
+      end
+    elsif params.main_domain == "fashiolista"
+      result[:source] = "#{params.main_domain}"
+      if params.path.start_with? "/item/"
+        m = /\/item\/([0-9]+).*/.match params.path
+        if m.length > 1 #found item id
+          result[:uid] = m[1] || result[:uid]
+        end
+      end
+    elsif params.main_domain == "shareasale"
+      result[:source] = "#{params.main_domain}"
+      unless params.query.nil?
+        result[:uid] = params.query_parameters["afftrack"].first unless params.query_parameters["afftrack"].first.nil? or params.query_parameters["afftrack"].first.empty?
+        result[:uid] = (result[:uid].split '--').first
+      end 
+    elsif params.main_domain == "facebook" #do not process facebook stats for now
+      result[:source] = "#{params.main_domain}"
+      result[:uid] = nil
+    end
+    result
   end
 end
